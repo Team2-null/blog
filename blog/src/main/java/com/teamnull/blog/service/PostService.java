befaa09eca01dabd405b67a8fa7935f2e09751ce
LeeEunseop <xxxnseop@gmail.com>
2023-01-03 15:17:07 +0900

    #6 [update] 게시글 작성, 조회, 수정, 삭제 기능 구현완료

--- blog/src/main/java/com/teamnull/blog/service/PostService.java
+++ blog/src/main/java/com/teamnull/blog/service/PostService.java
@@ -1,6 +1,5 @@
 package com.teamnull.blog.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,7 +7,9 @@ import javax.servlet.http.HttpServletRequest;
 import com.teamnull.blog.dto.post.request.PostCreateRequestDto;
 import com.teamnull.blog.dto.post.request.PostUpdateRequestDto;
 import com.teamnull.blog.dto.post.response.PostGetResponseDto;
+import com.teamnull.blog.dto.post.response.ResponseDto;
 import com.teamnull.blog.entity.User;
+import com.teamnull.blog.entity.enums.UserRoleEnum;
 import com.teamnull.blog.repository.CommentRepository;
 import com.teamnull.blog.repository.UserRepository;
 import com.teamnull.blog.util.jwt.JwtUtil;
@@ -23,7 +24,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor // 얘가 있어야 final이 붙어있는 레퍼지토리 필드에 연결
-public class PostService implements PostServiceInterface{
+public class PostService implements PostServiceInterface {
     private final PostRepository postRepository;
     private final JwtUtil jwtUtil;
     private final UserRepository userRepository;
@@ -31,7 +32,7 @@ public class PostService implements PostServiceInterface{
 
 
     // 게시글 작성
-    public Post createPost(PostCreateRequestDto postCreateRequestDto, HttpServletRequest request)  {
+    public Post createPost(PostCreateRequestDto postCreateRequestDto, HttpServletRequest request) {
         String token = jwtUtil.resolveToken(request);
         Claims claims = null;
 
@@ -52,13 +53,9 @@ public class PostService implements PostServiceInterface{
 
     // 게시글 전체 조회
     @Transactional(readOnly = true)
-    public List<PostGetResponseDto> getAllPost() {
+    public List<Post> getAllPost() {
         List<Post> postList = postRepository.findAllByOrderByCreateAtDesc();
-        List<PostGetResponseDto> postGetResponseDtoList = new ArrayList<>();
-        for (Post post : postList) {
-            postGetResponseDtoList.add(new PostGetResponseDto(post));
-        }
-        return postGetResponseDtoList;
+        return postList;
     }
 
     // 게시글 선택 조회
@@ -74,25 +71,59 @@ public class PostService implements PostServiceInterface{
         Post post = postRepository.findById(id).orElseThrow(
                 () -> new IllegalArgumentException("조회하신 아이디의 게시글이 없습니다.")
         );
-        if (post.isValidPassword(postUpdateRequestDto.getPassword())) {
-            post.updatePost(postUpdateRequestDto);
-            postRepository.save(post);
-        } else {
-            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        String token = jwtUtil.resolveToken(request);
+        Claims claims = null;
+
+        if (token != null) {
+            if (jwtUtil.validateToken(token)) {
+                claims = jwtUtil.getUserInfoFromToken(token);
+            } else {
+                throw new IllegalArgumentException("토큰이 유효하지 않습니다.");
+            }
         }
+        User user = userRepository.findByUsername(claims.getSubject()).orElseThrow(
+                () -> new IllegalArgumentException("회원을 찾을 수 없습니다.")
+        );
+        if (user.getRole() == UserRoleEnum.ADMIN) {
+            post.updatePost(postUpdateRequestDto);
+            return post;
+        }
+        post = postRepository.findByIdAndUserId(id, user.getId()).orElseThrow(
+                () -> new IllegalArgumentException("작성자만 수정이 가능합니다.")
+        );
+        post.updatePost(postUpdateRequestDto);
         return post;
+
+
     }
 
     // 게시글 삭제하기
-    public String deletePost(Long id, String password, HttpServletRequest request) {
+    public ResponseDto deletePost(Long id, HttpServletRequest request) throws ResponseDto {
         Post post = postRepository.findById(id).orElseThrow(
-                () -> new IllegalArgumentException("조회하신 아이디의 게시글이 없습니다.")
+                () -> new ResponseDto("조회하신 아이디의 게시글이 없습니다.")
         );
-        if (post.isValidPassword(password)) {
-            postRepository.deleteById(id);
-        } else {
-            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        ResponseDto response = new ResponseDto("게시물삭제가 완료되었습니다.");
+        String token = jwtUtil.resolveToken(request);
+        Claims claims = null;
+
+        if (token != null) {
+            if (jwtUtil.validateToken(token)) {
+                claims = jwtUtil.getUserInfoFromToken(token);
+            } else {
+                throw new ResponseDto("토큰이 유효하지 않습니다");
+            }
         }
-        return "삭제 완료";
+        User user = userRepository.findByUsername(claims.getSubject()).orElseThrow(
+                () -> new ResponseDto("회원을 찾을 수 없습니다")
+        );
+        if (user.getRole() == UserRoleEnum.ADMIN) {
+            postRepository.delete(post);
+            return response;
+        }
+        post = postRepository.findByIdAndUserId(id, user.getId()).orElseThrow(
+                () -> new ResponseDto("작성자만 삭제 가능합니다")
+        );
+        postRepository.deleteById(id);
+        return response;
     }
 }
